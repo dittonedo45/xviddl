1223edb439896c9823cd7e6eaa4a3b4befd23c17
Eddington Chamunorwa <dittonedo45@github.com>
2021-12-13 12:58:20 +0200

    ...

--- vidx.c
+++ vidx.c
@@ -1,39 +1,36 @@
-/*XXX This Document was modified on 1639294215 */
 #include <xnxx.h>
-#include <getopt.h>
 
 json_t *murl = 0;
 
-void err_s_l ( void *d, xmlError * g )
+void err_s_l(void *d, xmlError * g)
 {
 }
 
-static void crap_ignore ( void *d, const char *fmt, ... )
+static void crap_ignore(void *d, const char *fmt, ...)
 {
 }
 
-int main ( argsc, args, env )
+int main(argsc, args, env)
 int argsc;
 char **args, **env;
 {
- char **p = args + 1;
- int is_high = 1;
+    char **p = args + 1;
+    int is_high = 1;
 
- /* I just hate the motherfucken logs of crap,
-    kidding (We do not need error but
-    * To be honey right ?!!
-  */
+    /* I just hate the motherfucken logs of crap, kidding (We do not need error but
+     * To be honey right ?!!
+     */
 
- xmlSetGenericErrorFunc ( 0, crap_ignore );
- xmlSetStructuredErrorFunc ( 0, err_s_l );
+    xmlSetGenericErrorFunc(0, crap_ignore);
+    xmlSetStructuredErrorFunc(0, err_s_l);
 
- while( p && *p ) {
-  if( vdx_load_url ( *p ) ) {
-   printf ( "Failed To Load %s.\n", *p );
-  }
-  p++;
- }
+    while (p && *p) {
+	if (vdx_load_url(*p)) {
+	    printf("Failed To Load %s.\n", *p);
+	}
+	p++;
+    }
 
- vdx_download ( is_high );
- return 0;
+    vdx_download(is_high);
+    return 0;
 }
